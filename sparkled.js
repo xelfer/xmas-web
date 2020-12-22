0a75eb8a165346aa41ecafc8bdf2f797b5caa2a8
nick <nick@oz.org>
2020-12-22 12:33:54 +1100

    user alerts

--- sparkled.js
+++ sparkled.js
@@ -3,20 +3,35 @@ const express = require('express')
 const app = express()
 const port = 80
 
-
 app.use(express.static('/var/www/sparkled/images'));
 
 app.use(express.static('static'))
 
+fs = require('fs');
 app.post('/playSequence/:id', (req, res) => {
-	console.log("posting to api, " + req.params.id)
+        // check if music sequence started within last 30 seconds
+	if (fs.existsSync('/var/www/sparkled/musicisplaying.txt')) {
+                var olddate = fs.readFileSync('/var/www/sparkled/musicisplaying.txt', 'utf8');
+                var currentdate = Math.floor(new Date().getTime() / 1000);
+                if (currentdate - olddate > 30) {
+			play(req);
+		} else {
+			// i need the alert the user somehow
+			//res.end(1)
+		}
+	} else {
+		play(req);		
+	}
+	res.end()
+})
+
+function play(req) {
 	axios.post('http://localhost:8080/api/player', {
 		action: 'PLAY_SEQUENCE',
 		sequenceId: req.params.id,
 		'repeat': false
-	})
+		})
 		.then((res) => {
-			fs = require('fs');
 			fs.writeFile('lastseq.txt', Math.floor(new Date().getTime() / 1000), function (err) {
 				if (err) return console.log(err);
 			});
@@ -27,13 +42,17 @@ app.post('/playSequence/:id', (req, res) => {
 			fs.appendFile('users.txt', ',', function (err) {
 				if (err) return console.log(err);
 			});
-		})
-		.catch((error) => {
-			console.error(error)
-		})
-	res.end()
-})
+			if (req.params.id == 14 || req.params.id == 59) {
+				fs.appendFile('musicisplaying.txt', ',', function (err) {
+					if (err) return console.log(err);
+				});
+			}
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+}
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`12Brian app listening at http://12brian.st`)
 })
