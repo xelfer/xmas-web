f0eadbe2bdd1266c71805b10b6a2a1dd46310e5d
Nicolas Triantafillou <nicolast@gmail.com>
2021-11-05 10:36:34 +1100

    Update xmas.js

--- xmas.js
+++ xmas.js
@@ -17,7 +17,7 @@ const server = app.listen(port, () => {
 const io = require('socket.io')(server);
 
 
-app.use(express.static('static'))
+app.use('/', express.static('static'))
 app.use('/watch', express.static('static/watch'))
 
 let lastRan = null;
